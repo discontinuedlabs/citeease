dcd75ba9e3bd85db5f789d8ec446aa38b81db76e
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-01-22 02:46:46 +0400

    Updates

---
+++ service-worker.js
@@ -0,0 +1,60 @@
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
+const self = this;
+
+const PRECACHE_URLS = [
+    "index.html",
+    "./",
+    "./static/js/main.chunk.js",
+    "./static/js/0.chunk.js",
+    "./static/js/bundle.js",
+    "./public/index.html",
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches
+            .open(PRECACHE)
+            .then((cache) => cache.addAll(PRECACHE_URLS))
+            .then(self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches
+            .keys()
+            .then((cacheNames) => {
+                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+            })
+            .then((cachesToDelete) => {
+                return Promise.all(
+                    cachesToDelete.map((cacheToDelete) => {
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then((cachedResponse) => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return caches.open(RUNTIME).then((cache) => {
+                    return fetch(event.request).then((response) => {
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
+    }
+});
