11faaffca997965527220f3ba291a9ba49ad7c5c
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-05-04 21:45:48 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,58 +1,53 @@
-var APP_PREFIX = "citeease-";
+var GHPATH = "/citeease";
+var APP_PREFIX = "citeease_";
 var VERSION = "v1";
+var URLS = [
+    `${GHPATH}/`,
+    `${GHPATH}/index.html`,
+    `${GHPATH}/static/js/main.chunk.js`,
+    `${GHPATH}/static/js/bundle.js`,
+    `${GHPATH}/static/js/0.chunk.js`,
+    `${GHPATH}/static/css/main.1fe0c195.css`,
+    `${GHPATH}/static/js/main.0fa6b5ab.js`,
+];
+
 var CACHE_NAME = APP_PREFIX + VERSION;
-var URLS = ["/citeease/", "/citeease/index.html", "/citeease/static/js/bundle.js", "/citeease/manifest.json"];
-
-var self = this;
-
-// Respond with cached resources
-self.addEventListener("fetch", function (event) {
-    console.log("fetch request : " + event.request.url);
-    event.respondWith(
-        caches.match(event.request).then(function (request) {
+self.addEventListener("fetch", function (e) {
+    console.log("Fetch request : " + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
             if (request) {
-                // if cache is available, respond with cache
-                console.log("responding with cache : " + event.request.url);
+                console.log("Responding with cache : " + e.request.url);
                 return request;
             } else {
-                // if there are no cache, try fetching request
-                console.log("file is not cached, fetching : " + event.request.url);
-                return fetch(event.request);
+                console.log("File is not cached, fetching : " + e.request.url);
+                return fetch(e.request);
             }
-
-            // You can omit if/else for console.log & put one line below like this too.
-            // return request || fetch(e.request)
         })
     );
 });
 
-// Cache resources
-self.addEventListener("install", function (event) {
-    event.waitUntil(
+self.addEventListener("install", function (e) {
+    e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
-            console.log("installing cache : " + CACHE_NAME);
+            console.log("Installing cache : " + CACHE_NAME);
             return cache.addAll(URLS);
         })
     );
 });
 
-// Delete outdated caches
-self.addEventListener("activate", function (event) {
-    event.waitUntil(
+self.addEventListener("activate", function (e) {
+    e.waitUntil(
         caches.keys().then(function (keyList) {
-            // `keyList` contains all cache names under your username.github.io
-            // filter out ones that has this app prefix to create white list
             var cacheWhitelist = keyList.filter(function (key) {
                 return key.indexOf(APP_PREFIX);
             });
-            // add current cache name to white list
             cacheWhitelist.push(CACHE_NAME);
-
             return Promise.all(
-                keyList.map(function (key, index) {
+                keyList.map(function (key, i) {
                     if (cacheWhitelist.indexOf(key) === -1) {
-                        console.log("deleting cache : " + keyList[index]);
-                        return caches.delete(keyList[index]);
+                        console.log("Deleting cache : " + keyList[i]);
+                        return caches.delete(keyList[i]);
                     }
                 })
             );
