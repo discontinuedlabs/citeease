089f6e7d2b77d5703442fc62677dd7ac5d2a966f
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-07-17 21:47:18 +0400

    bug fixes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
     return {
-        base: "/citeease/",
+        base: "/citeease",
 
         build: {
             outDir: "build",
