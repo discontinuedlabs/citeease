eb599cfe3ac44c1fcdc7ec196a9d950ec422bc75
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-07-18 23:43:25 +0400

    sw test

--- src/service-worker.js
+++ src/service-worker.js
@@ -6,27 +6,27 @@ import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
 import { StaleWhileRevalidate } from "workbox-strategies";
 
+console.log("from service-worker.js");
+
 clientsClaim();
 
-precacheAndRoute([...self.__WB_MANIFEST]);
+precacheAndRoute([...this.__WB_MANIFEST]);
 
 const fileExtensionRegexp = /[^?/]+\.[^/]+$/;
 
 registerRoute(
-    // Return false to exempt requests from being fulfilled by index.html.
     ({ request, url }) => {
-        // If this isn't a navigation, skip.
         if (request.mode !== "navigate") {
             return false;
-        } // If this is a URL that starts with /_, skip.
+        }
 
         if (url.pathname.startsWith("/_")) {
             return false;
-        } // If this looks like a URL for a resource, because it contains // a file extension, skip.
+        }
 
         if (url.pathname.match(fileExtensionRegexp)) {
             return false;
-        } // Return true to signal that we want to use the handler.
+        }
 
         return true;
     },
