4e5139cb711d6fce7783e793ec6418b3e5a3edae
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-07-19 01:02:49 +0400

    vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,47 @@ export default defineConfig(({ mode }) => {
             outDir: "build",
             assetsDir: ".",
         },
-        plugins: [react(), VitePWA()],
+        plugins: [
+            react(),
+            VitePWA({
+                registerType: "autoUpdate",
+                workbox: {
+                    clientsClaim: true,
+                    skipWaiting: true,
+                },
+                devOptions: {
+                    enabled: true,
+                },
+                manifest: {
+                    short_name: "CiteEase",
+                    name: "CiteEase: Your Citation Companion",
+                    description:
+                        "CiteEase is a powerful and user-friendly open-source citation management app designed for students, researchers, and anyone who values efficiency and organization.",
+                    icons: [
+                        {
+                            src: "/citeease/favicon.ico",
+                            sizes: "64x64 32x32 24x24 16x16",
+                            type: "image/x-icon",
+                        },
+                        {
+                            src: "/citeease/images/favicons/android-chrome-192x192.png",
+                            sizes: "192x192",
+                            type: "image/png",
+                        },
+                        {
+                            src: "/citeease/images/favicons/android-chrome-512x512.png",
+                            sizes: "512x512",
+                            type: "image/png",
+                        },
+                    ],
+                    scope: "/citeease/",
+                    start_url: "/citeease/",
+                    display: "standalone",
+                    orientation: "portrait",
+                    theme_color: "#f6f8f9",
+                    background_color: "#f6f8f9",
+                },
+            }),
+        ],
     };
 });
