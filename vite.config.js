f829c6f9004041425ff046b246885af259b52ccf
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-07-31 00:16:29 +0400

    bug fixes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(({ mode }) => {
-    const { VITE_PUBLIC_URL } = loadEnv(mode, process.cwd());
-
     return {
         server: {
             port: 3000,
         },
 
-        base: VITE_PUBLIC_URL,
+        base: "/citeease/",
 
         build: {
             outDir: "build",
