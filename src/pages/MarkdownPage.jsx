88dc54b8c5e46a7732b1db227c89f66fb9e8c87b
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-08-13 22:37:39 +0400

    added TopBar to MarkdownPage

--- src/pages/MarkdownPage.jsx
+++ src/pages/MarkdownPage.jsx
@@ -1,23 +1,32 @@
 import { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
+import { TopBar } from "../components/ui/MaterialComponents";
 
-export default function MarkdownPage(props) {
-    const { title, filePath } = props;
+export default function MarkdownPage({ title = undefined, filePath }) {
     const [content, setContent] = useState("");
+    const [firstHeading, setFirstHeading] = useState("");
 
     useEffect(() => {
         async function fetchContent() {
             const response = await fetch(filePath);
             const text = await response.text();
-            setContent(text);
+
+            const headings = text.split("\n").filter((line) => line.startsWith("#"));
+            const firstHeadingMatch = headings.find((heading) => heading.startsWith("# "));
+            if (firstHeadingMatch && !title) {
+                setContent(text.replace(firstHeadingMatch, ""));
+                setFirstHeading(firstHeadingMatch.trim().slice(2));
+            } else {
+                setContent(text);
+            }
         }
         fetchContent();
     }, [filePath]);
 
     return (
-        <div className="mx-auto max-w-[50rem] p-4">
-            <h1 className="hidden">{title}</h1> {/* For screen readers and tab title */}
-            <ReactMarkdown>{content}</ReactMarkdown>
+        <div className="mx-auto max-w-[50rem]">
+            <TopBar headline={title || firstHeading} />
+            <ReactMarkdown className="p-4">{content}</ReactMarkdown>
         </div>
     );
 }
