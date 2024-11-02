e49222c498efe17f6046136726482763172bbe22
Yusef Almamari <yu9811sef.ya@gmail.com>
2024-11-02 22:35:12 +0400

    Fix sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,5 @@
+// Import Workbox library from CDN
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js");
-
-workbox.setConfig({ debug: false });
 
 const { core, routing, strategies, cacheableResponse, expiration } = workbox;
 
@@ -10,26 +9,32 @@ const { NetworkFirst, CacheFirst, StaleWhileRevalidate } = strategies;
 const { CacheableResponsePlugin } = cacheableResponse;
 const { ExpirationPlugin } = expiration;
 
+// Listen for 'SKIP_WAITING' message and activate new service worker immediately
 self.addEventListener("message", (event) => {
     if (event.data && event.data.type === "SKIP_WAITING") {
         self.skipWaiting();
     }
 });
 
+// Claim control of clients immediately after the service worker activates
 clientsClaim();
 
+// Cache page navigation requests using a 'Network First' strategy
+// This tries the network first, and falls back to the cache if offline
 registerRoute(
     ({ request }) => request.mode === "navigate",
     new NetworkFirst({
         cacheName: "pages",
         plugins: [
             new CacheableResponsePlugin({
-                statuses: [200],
+                statuses: [200], // Only cache successful responses
             }),
         ],
     })
 );
 
+// Cache CSS, JavaScript, and Web Worker files using a 'Stale While Revalidate' strategy
+// This serves cached assets quickly and updates the cache in the background
 registerRoute(
     ({ request }) =>
         request.destination === "style" || request.destination === "script" || request.destination === "worker",
@@ -37,23 +42,25 @@ registerRoute(
         cacheName: "assets",
         plugins: [
             new CacheableResponsePlugin({
-                statuses: [200],
+                statuses: [200], // Only cache successful responses
             }),
         ],
     })
 );
 
+// Cache image requests using a 'Cache First' strategy
+// This serves images from the cache and updates only when necessary
 registerRoute(
     ({ request }) => request.destination === "image",
     new CacheFirst({
         cacheName: "images",
         plugins: [
             new CacheableResponsePlugin({
-                statuses: [200],
+                statuses: [200], // Only cache successful responses
             }),
             new ExpirationPlugin({
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24 * 30,
+                maxEntries: 50, // Limit to 50 images in the cache
+                maxAgeSeconds: 60 * 60 * 24 * 30, // Cache for 30 days
             }),
         ],
     })
--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,3 +1,4 @@
+// Redister the service worker
 export function register() {
     if ("serviceWorker" in navigator) {
         const publicUrl = import.meta.env.VITE_PUBLIC_URL;
@@ -13,6 +14,7 @@ export function register() {
     }
 }
 
+// Unregister the service worker
 export function unregister() {
     if ("serviceWorker" in navigator) {
         navigator.serviceWorker.ready
